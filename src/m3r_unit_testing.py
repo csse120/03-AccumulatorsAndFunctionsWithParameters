18994ea75ecd5e1fec5370d8d5355facaf8f1d45
DavidMutchler <mutchler@rose-hulman.edu>
2019-09-09 12:21:02 -0400

    Fixed TODO numbering, other changes of no substance.

--- src/m3r_unit_testing.py
+++ src/m3r_unit_testing.py
@@ -8,11 +8,11 @@ Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher, Mark Hays,
 """  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
 ###############################################################################
 #
-# TODO: 1.
+# TODO: 2.  If you have not already done so:
 #   Allow this module to use the  rosegraphics.py  module by marking the
 #     src
 #   folder in this project as a "Sources Root", as follows:
-#
+#     ____
 #     In the Project window (to the left), right click on the src  folder,
 #     then select  Mark Directory As  ~  Sources Root.
 #
@@ -55,6 +55,8 @@ def distance(point):
     Side effects:   None.
     Example:
       If the argument is  rg.Point(3, 4)  this function returns 5.
+    Type hints:
+      :type point rg.Point
     """
     # This code has an error, on purpose.  Do NOT fix it.
     x_squared = point.x * point.x
@@ -69,19 +71,19 @@ def distance(point):
 main()
 
 ###############################################################################
-# TODO: 2.
+# TODO: 3.
+#   ___
+#   READ the following, asking questions as needed.
+#   When you believe that you understand what is says about UNIT TESTING,
+#   mark the above _TODO_ as DONE.
 #
-# READ the following, asking questions as needed.
-# When you believe that you understading what is says about UNIT TESTING,
-# mark the above TO-DO as DONE.
-#
-#   In most exercises we will follow the  UNIT TESTING
-#   that the above code illustrates.
+# In most exercises we will follow the  UNIT TESTING
+# that the above code illustrates.
 #
 #   Look at the  DISTANCE  function defined above.
 #   It is the function that we want to test.  Read its doc-string.
 #
-#   Now look at the  run_test_DISTANCE  function defined above.
+#   Now look at the  RUN_TEST_DISTANCE  function defined above.
 #   It is the function that  TESTS  the DISTANCE function.
 #   We call this  UNIT TESTING  because we are testing a single UNIT
 #   of the program, here, the function DISTANCE.
@@ -92,25 +94,25 @@ main()
 #         on a particular test case, usually by working the problem
 #         by hand or by trusting a test case provided by the instructor.
 #
-#         For example, in the above   run_test_DISTANCE   function,
+#         For example, in the above   run_test_distance   function,
 #         the human tester figured out, by doing the problem by hand,
 #         that the distance that (3, 4) is from (0, 0) is 5:
 #             expected = 5
 #
-#     2.  The  run_test_DISTANCE  function CALLS the function to test,
-#         sending it the test case the human tester chose:
+#     2.  The  run_test_distance  function CALLS the function to test,
+#         sending it the test case the human tester chose, like this:
 #             answer = distance(rg.Point(3, 4))
 #
 #         The code CAPTURES the returned value in a variable (namely, answer).
 #
-#     3.  The  run_test_DISTANCE  function then PRINTS both the EXPECTED
+#     3.  The  run_test_distance  function then PRINTS both the EXPECTED
 #         answer (5 in our example) and the ACTUAL answer returned
 #         (the value of the variable answer).
 #             print('Test 3 expected:', expected)
 #             print('       actual:  ', answer)
 #
 # The above forms a single TEST for a function that returns a value.
-# When the software developer / tester runs the run_test_DISTANCE function,
+# When the software developer / tester runs the run_test_distance function,
 # she compares the EXPECTED and ACTUAL values that are printed.
 #   -- If they are the same, the code PASSED the test.
 #   -- If they are different, the code FAILED the test.
@@ -138,6 +140,4 @@ main()
 # We are teaching you practices that scale up, so we demand that most
 # of the functions that you write from here on RETURN a value instead
 # of PRINTing it.
-#
-
 ###############################################################################
