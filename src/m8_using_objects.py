46b9b147597cc98143ad0e856c13d7f9b14849c0
DavidMutchler <mutchler@rose-hulman.edu>
2019-09-09 18:27:07 -0400

    Reordered TODOs to make more sense.

--- src/m8_using_objects.py
+++ src/m8_using_objects.py
@@ -36,6 +36,40 @@ def two_circles():
     # -------------------------------------------------------------------------
 
 
+def lines():
+    """
+    -- Constructs a rg.RoseWindow.
+    -- Constructs and draws on the window two rg.Lines such that:
+          -- They both fit in the window and are easily visible.
+          -- One rg.Line has the default thickness.
+          -- The other rg.Line is thicker (i.e., has a bigger width).
+    -- Uses a rg.Line method to get the midpoint (center) of the
+         thicker rg.Line.
+    -- Then prints (on the console, on SEPARATE lines, for the thicker Line):
+         -- the midpoint itself
+         -- the x-coordinate of the midpoint
+         -- the y-coordinate of the midpoint
+
+       Here is an example of the output on the console, if the two
+       endpoints of the thicker line are at (100, 100) and (121, 200):
+            Point(110.5, 150.0)
+            110.5
+            150.0
+
+    -- Waits for the user to press the mouse, then closes the window.
+    """
+    # -------------------------------------------------------------------------
+    # TODO: 3. Implement this function, per its green doc-string above.
+    #    -- ANY lines that meet the criteria are fine.
+    #  Put a statement in   main   to test this function
+    #    (by calling this function).
+    #   HINT: Module  m2r_using_rosegraphics  has helpful examples for this.
+    #  ___
+    #  IMPORTANT: Use the DOT TRICK to guess the name of the relevant method
+    #    and instance variables.
+    # -------------------------------------------------------------------------
+
+
 def circle_and_rectangle():
     """
     -- Constructs an rg.RoseWindow.
@@ -69,7 +103,7 @@ def circle_and_rectangle():
            150.0
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this function, per its green doc-string above.
+    # TODO: 4. Implement this function, per its green doc-string above.
     #    -- ANY objects that meet the criteria are fine.
     #  Put a statement in   main   to test this function
     #    (by calling this function).
@@ -78,31 +112,6 @@ def circle_and_rectangle():
     #  IMPORTANT: Use the DOT TRICK to guess the names of the relevant
     #       instance variables for outline thickness, etc.
     # -------------------------------------------------------------------------
-
-
-def lines():
-    """
-    -- Constructs a rg.RoseWindow.
-    -- Constructs and draws on the window two rg.Lines such that:
-          -- They both fit in the window and are easily visible.
-          -- One rg.Line has the default thickness.
-          -- The other rg.Line is thicker (i.e., has a bigger width).
-    -- Uses a rg.Line method to get the midpoint (center) of the
-         thicker rg.Line.
-    -- Then prints (on the console, on SEPARATE lines):
-         -- the midpoint itself
-         -- the x-coordinate of the midpoint
-         -- the y-coordinate of the midpoint
-
-       Here is an example of the output on the console, if the two
-       endpoints of the thicker line are at (100, 100) and (121, 200):
-            Point(110.5, 150.0)
-            110.5
-            150.0
-
-    -- Waits for the user to press the mouse, then closes the window.
-    """
-    # TODO: 4. Implement and test this function.
 
 
 # -----------------------------------------------------------------------------
